9e96adba197306bf53706cfb174177f8224cb9e8
Marcos Cordeiro <marcosc974@gmail.com>
2024-12-26 21:52:42 -0300

    Atualização do autoload

--- autoload.php
+++ autoload.php
@@ -2,6 +2,15 @@
 /**
  * Arquivo responsável por processar as requisições automáticas dos arquivos do projeto
  */
-spl_autoload_register(function($classe){
-    require $classe.".php";
+spl_autoload_register(function ($class) {
+    $baseDir = __DIR__ . '/'; // Diretório base do projeto
+    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+    
+    if (file_exists($file)) {
+        
+        require $file;
+
+    } else {
+        die("Erro: Arquivo $file não encontrado!");
+    }
 });
